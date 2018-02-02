8c66ed8d673c8f7d3065fa2c048a3f339a58d642
楊竑昕 <jk195417@gmail.com>
2018-02-02 17:14:05 +0800

    anchor CRUD

---
+++ app/controllers/anchors_controller.rb
@@ -0,0 +1,48 @@
+class AnchorsController < ApplicationController
+  before_action :set_anchor, only: %i[show edit update destroy]
+
+  def index
+    @anchors = Anchor.all
+  end
+
+  def show; end
+
+  def new
+    @anchor = Anchor.new
+  end
+
+  def edit; end
+
+  def create
+    @anchor = Anchor.new(anchor_params)
+    if @anchor.save
+      redirect_to @anchor, notice: 'Anchor was successfully created.'
+    else
+      flash[:warning] = 'create failed'
+      render :new
+    end
+  end
+
+  def update
+    if @anchor.update(anchor_params)
+      redirect_to @anchor, notice: 'Anchor was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @anchor.destroy
+    redirect_to anchors_url, notice: 'Anchor was successfully destroyed.'
+  end
+
+  private
+
+  def set_anchor
+    @anchor = Anchor.find(params[:id])
+  end
+
+  def anchor_params
+    params.require(:anchor).permit(:latitude, :longitude, :elevation, :what_3_words)
+  end
+end
