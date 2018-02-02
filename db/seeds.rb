e8371a9c1f323778224ca454541fbd87c0cc0875
楊竑昕 <jk195417@gmail.com>
2018-02-02 17:12:10 +0800

    load csv to db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'csv'
+
+anchor_group_counts = 0
+csv1 = Rails.root.join 'lib', 'seeds', 'represent_anchors.csv'
+CSV.foreach(csv1, headers: true) do |row|
+  ag = AnchorGroup.new
+  ag.latitude = row['lat']
+  ag.longitude = row['lng']
+  ag.old_name  = row['group']
+  ag.what_3_words = row['what_3_words']
+  ag.elevation = row['elevation']
+  anchor_group_counts += 1 if ag.save
+end
+puts "Create #{anchor_group_counts} anchor_groups from #{csv1}"
+
+anchor_counts = 0
+csv2 = Rails.root.join 'lib', 'seeds', 'unnamed.csv'
+CSV.foreach(csv2, headers: true) do |row|
+  a = Anchor.new
+  a.latitude = row['lat'].to_f
+  a.longitude = row['lon'].to_f
+  a.anchor_group = AnchorGroup.find_or_create_by!(old_name: row['anchor_group'])
+  anchor_counts += 1 if a.save
+end
+puts "Create #{anchor_counts} anchor_groups from #{csv2}"
