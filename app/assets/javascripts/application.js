6d99caef549de4ad4ffd45e415372067d537ebd3
楊竑昕 <jk195417@gmail.com>
2018-02-03 22:01:16 +0800

    disable turbolinks

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,6 @@
 // about supported directives.
 //
 //= require rails-ujs
-//= require turbolinks
 //= require jquery/dist/jquery
 //= require popper.js/dist/umd/popper
 //= require bootstrap/dist/js/bootstrap
