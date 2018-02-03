4cc84f656043074ef40b7fee093c3bc8567cb91c
楊竑昕 <jk195417@gmail.com>
2018-02-03 21:57:18 +0800

    pages#solutions

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :anchor_groups
   root to: 'pages#index'
   get 'map', to: 'pages#map'
+  get 'solutions', to: 'pages#solutions'
   resources :anchors
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
