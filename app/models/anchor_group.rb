0d563a4e9b1c5a0ebdb0c545f8e901aa4de105c5
楊竑昕 <jk195417@gmail.com>
2018-02-03 12:14:24 +0800

    add more info

--- app/models/anchor_group.rb
+++ app/models/anchor_group.rb
@@ -4,4 +4,14 @@ class AnchorGroup < ApplicationRecord
   validates_presence_of :latitude, :longitude
   validates :latitude, numericality: { greater_than_or_equal_to:  -90, less_than_or_equal_to:  90 }
   validates :longitude, numericality: { greater_than_or_equal_to: -180, less_than_or_equal_to: 180 }
+
+  def closest_anchor
+    ary = []
+    anchors.each { |a| ary << distance_square_to(a.latitude, a.longitude) }
+    anchors[ary.index(ary.max)]
+  end
+
+  def distance_square_to(lat, lng)
+    (latitude - lat).abs**2 + (longitude - lng).abs**2
+  end
 end
