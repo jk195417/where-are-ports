d5b46ca9bb94c00836cca1a7ea7fd57edf80f0cd
楊竑昕 <jk195417@gmail.com>
2018-02-04 02:19:19 +0800

    admin can manage all

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,7 +2,10 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
+    user ||= User.new
+    user.roles
     can :read, :all
+    can :manage, :all if user.has_cached_role? :admin
     # Define abilities for the passed in user here. For example:
     #
     #   user ||= User.new # guest user (not logged in)
